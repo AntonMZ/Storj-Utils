4935aaf978e62f7ec037812ac61c684956a4e89a
Dmitri Don <fotkin@gmail.com>
2017-06-08 02:46:19 +0300

    fixed multiple IPs output for FreeBSD

--- v.1.0.2/health.sh
+++ v.1.0.2/health.sh
@@ -19,7 +19,7 @@ DATE=$(date)
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	IP=$(hostname -I)
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
-	IP=`ifconfig | grep "inet " | grep -v "inet6"  | grep -v "127.0.0.1" | awk '{print $2}'`
+	IP=`ifconfig | grep "inet " | grep -v "inet6"  | grep -v "127.0.0.1" | awk '{print $2}' | tr '\n' ' '`
 fi
 
 SESSIONS=$(netstat -np | grep node | grep tcp | wc -l)
